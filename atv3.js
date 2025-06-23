2c314aa333ee8236c5acf3b9422ff4bdfcbc32f3
gabyforcione <gabriellyforcione@colegiopaulodetarso.com.br>
2025-06-23 16:13:28 +0000

    ggg

---
+++ atv3.js
@@ -0,0 +1,12 @@
+const pessoa = {
+  nome: "Gabrielly",
+  idade: 18,
+  hobbies: ["dormir", "sair com amigos"],
+  endereco: { rua: "Mazzine", numero: "190"  }
+};
+
+console.log(pessoa.nome);
+console.log(pessoa.idade);
+console.log(pessoa.hobbies);
+console.log(pessoa.hobbies[0]);
+console.log(pessoa.endereco.rua);
